44219d6ca0e144507770afcc3ebd88d233ddd5eb
Андрей Евстигнеев <Grandvil1999@mail.ru>
2022-09-22 19:11:35 +0300

    Добавлен метод поиска

--- src/main/java/com/example/LR1/Lr1Application.java
+++ src/main/java/com/example/LR1/Lr1Application.java
@@ -30,8 +30,70 @@ public class Lr1Application {
         SpringApplication.run(Lr1Application.class, args);
     }
 
+    @GetMapping("/search")
+    public String search(@RequestParam(value = "search", defaultValue = "") String search) throws IOException, JAXBException {
+        File file = new File("src/main/resources/example2.xml");
+
+        // Валидация
+        boolean ok = schemaValidator();
+        SystemXML systemXML = new SystemXML();
+
+        if (ok) {
+            JAXBContext jaxbContext = JAXBContext.newInstance(SystemXML.class);
+
+            // Анмаршалинг
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            systemXML = (SystemXML) jaxbUnmarshaller.unmarshal(file);
+
+            // Меняем объект
+            systemXML.personList.personList.get(0).firstName = "Anton";
+            systemXML.personList.personList.get(0).lastName = "Antonov";
+            systemXML.personList.personList.get(0).pnoneNumber = "12345";
+            systemXML.chatHistory.chatList.get(1).messageList.messageList.get(0).content.body = "NEW MESSAGE";
+
+            // Создаём XML с измененными данными
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            jaxbMarshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, "src/main/resources/XMLScheme.xsd");
+
+            OutputStream os = new FileOutputStream(".\\myxml.xml");
+            jaxbMarshaller.marshal(systemXML, os);
+
+            try (FileWriter writer = new FileWriter("myjson.json", false)) {
+                writer.write(new Gson().toJson(systemXML));
+                writer.flush();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            for (Person i : systemXML.personList.personList) {
+                if (i.lastName.equals(search)) {
+                    return new Gson().toJson(i);
+                }
+            }
+            return "Нет пользователя";
+        }
+        return new Gson().toJson(systemXML);
+    }
+
+    public static boolean schemaValidator() {
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File(".\\src\\main\\resources\\XMLScheme.xsd"));
+            Validator validator = schema.newValidator();
+            XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(Files.newInputStream(Paths.get(".\\src\\main\\resources\\example2.xml")));
+            validator.validate(new StAXSource(reader));
+
+            System.out.println("XML is valid");
+            return true;
+        } catch (Exception e) {
+            System.out.println("XML is not valid");
+            return false;
+        }
+    }
+
     @GetMapping("/")
-    public String first(@RequestParam(value = "name", defaultValue = "World") String name) throws IOException, JAXBException {
+    public String list() throws IOException, JAXBException {
         File file = new File("src/main/resources/example2.xml");
 
         // Валидация
@@ -69,20 +131,6 @@ public class Lr1Application {
         }
         return new Gson().toJson(systemXML);
     }
-
-    public static boolean schemaValidator() {
-        try {
-            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(new File(".\\src\\main\\resources\\XMLScheme.xsd"));
-            Validator validator = schema.newValidator();
-            XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(Files.newInputStream(Paths.get(".\\src\\main\\resources\\example2.xml")));
-            validator.validate(new StAXSource(reader));
-
-            System.out.println("XML is valid");
-            return true;
-        } catch (Exception e) {
-            System.out.println("XML is not valid");
-            return false;
-        }
-    }
 }
+
+
